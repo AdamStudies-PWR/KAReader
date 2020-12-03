e6e01d793e846700d73a4567c3c6762358cf8c4a
Adam Krizar <adam.krizar@gmail.com>
2020-12-03 17:01:22 +0100

    Loading sample files

--- iComicPRO/app/src/main/java/com/example/icomicpro/MainActivity.java
+++ iComicPRO/app/src/main/java/com/example/icomicpro/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.icomicpro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -17,6 +20,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -25,14 +29,14 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.valueOf;
+
 public class MainActivity extends AppCompatActivity
 {
     boolean firstTime = true; //temporary
     TextView opened = null;
-    LinearLayout newLL = null;
 
-    final String comicDir = "Komiksy";
-    final String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+    final String PATH = "Comics";
 
     ArrayList<Series> comics = new ArrayList<Series>();
 
@@ -42,22 +46,70 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        requestPermissions(permissions, 1);
-        loadContent();
-
-        //       SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE);
-
-        //       if(sharedPreferences.getBoolean(getString(R.string.first_time_key), true))
-        if (firstTime) firstTime();
-
-            //------------------------------------------------------------------------------------------
-            // kinda ugly and temporary but i'll make it dynamic
-            //------------------------------------------------------------------------------------------
-
-        else
+        if (firstTime)
         {
-            appearifySeries();
+            Log.e("First time", "entered");
+            if (loadComics()) appearifySeries();
+            else handleCriticalErrors();
+
+            firstTime = false;
+        } else appearifySeries();
+    }
+
+    boolean loadComics()
+    {
+        Context context = getApplicationContext();
+        Series temp;
+        File[] temp2;
+        List<String> temp4 = new ArrayList<>();
+
+        File comicDir = new File(context.getExternalFilesDir(null), PATH);
+
+        comics.clear();
+
+        if (!comicDir.exists())
+        {
+            if (!comicDir.mkdirs()) return false;
         }
+
+        File[] files = comicDir.listFiles();
+        if (files != null)
+        {
+            for (File file : files)
+            {
+                if (file.isDirectory())
+                {
+                    temp = new Series();
+                    temp.title = file.getName();
+                    temp2 = file.listFiles();
+                    if (temp2 != null)
+                    {
+                        temp4.clear();
+                        for (File temp3 : temp2)
+                        {
+                            if (!temp3.isDirectory())
+                            {
+                                temp4.add(temp3.getName().split("[.]")[0]);
+                            }
+                        }
+                        temp.issues.addAll(temp4);
+                    }
+                    comics.add(temp);
+                }
+            }
+        }
+
+        for (Series ser : comics)
+        {
+            Log.e("Contents: ", ser.title + " " + String.valueOf(ser.issues.size()));
+        }
+
+        return true;
+    }
+
+    void handleCriticalErrors()
+    {
+        //todo
     }
 
     void appearifySeries()
@@ -66,99 +118,93 @@ public class MainActivity extends AppCompatActivity
 
         scrollLayout.setPadding(40, 40, 40, 40);
 
-        Button button = new Button(this);
-        button.setText("zamknieta seria 1");
-        button.setBackground(getDrawable(R.drawable.gradient));
-        button.setTextSize(20);
-        button.setTextColor(getColor(R.color.colorText));
-        button.setHeight(70);
-        button.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
-        button.setPadding(40, 20, 20, 20);
-        scrollLayout.addView(button);
-
-        final Button button2 = new Button(this);
-        button2.setText("zamknieta seria 2");
-        button2.setBackground(getDrawable(R.drawable.gradient));
-        button2.setTextSize(20);
-        button2.setTextColor(getColor(R.color.colorText));
-        button2.setHeight(70);
-        button2.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
-        button2.setPadding(40, 20, 20, 20);
-        scrollLayout.addView(button2);
-
-        final ScrollView newSV = new ScrollView(this);
-        newLL = new LinearLayout(this);
-        newLL.setOrientation(LinearLayout.VERTICAL);
-        newLL.setBackgroundColor(getColor(R.color.colorAccent));
-        newSV.addView(newLL);
-
-        scrollLayout.addView(newSV, scrollLayout.indexOfChild(button2) + 1); //fix this adding a new divider!!
-
-        button2.setOnClickListener(new View.OnClickListener()
+        for(Series series : comics)
         {
-            @Override
-            public void onClick(View v)
-            {
-                final TextView letssee = new TextView(v.getContext());
-                letssee.setText("> rozdzial " + newLL.getChildCount());
-                if ((newLL.getChildCount() % 2 == 0))
-                {
-                    letssee.setBackgroundColor(getColor(R.color.colorAccent));
-                } else
-                {
-                    letssee.setBackgroundColor(getColor(R.color.colorAccent2));
-                }
-                letssee.setTextSize(16);
-                letssee.setTextColor(getColor(R.color.colorText));
-                letssee.setPadding(60, 20, 0, 20);
-                newLL.addView(letssee);
+            final Button comic = new Button(this);
+            comic.setText(series.title);
+            comic.setBackground(getDrawable(R.drawable.gradient));
+            comic.setTextSize(20);
+            comic.setTextColor(getColor(R.color.colorText));
+            comic.setHeight(70);
+            comic.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            comic.setPadding(40, 20, 20, 20);
+            scrollLayout.addView(comic);
 
-                letssee.setOnClickListener(new View.OnClickListener()
+            final ScrollView newSV = new ScrollView(this);
+            final LinearLayout newLL = new LinearLayout(this);
+            newLL.setOrientation(LinearLayout.VERTICAL);
+            newLL.setBackgroundColor(getColor(R.color.colorAccent));
+            newSV.setVisibility(View.GONE);
+            newSV.addView(newLL);
+
+            if (!series.issues.isEmpty())
+            {
+                for(String title : series.issues)
                 {
-                    @Override
-                    public void onClick(View v)
+                    final TextView letssee = new TextView(comic.getContext());
+                    letssee.setText(title);
+                    if ((newLL.getChildCount() % 2 == 0))
                     {
-                        letssee.setBackgroundColor(getColor(R.color.colorText));
-                        letssee.setTextColor(getColor(R.color.colorAccent));
-
-                        opened = letssee;
-                        goToChapter(newLL.indexOfChild(letssee));
+                        letssee.setBackgroundColor(getColor(R.color.colorAccent));
+                    } else
+                    {
+                        letssee.setBackgroundColor(getColor(R.color.colorAccent2));
                     }
-                });
+                    letssee.setTextSize(16);
+                    letssee.setTextColor(getColor(R.color.colorText));
+                    letssee.setPadding(60, 20, 0, 20);
+                    newLL.addView(letssee);
 
-                newLL.setPadding(0, 0, 0, 20);
+                    letssee.setOnClickListener(new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            letssee.setBackgroundColor(getColor(R.color.colorText));
+                            letssee.setTextColor(getColor(R.color.colorAccent));
 
-                button2.setText("otwarta seria");
+                            opened = letssee;
+                            goToChapter(newLL.indexOfChild(letssee));
+                        }
+                    });
 
-            }
-        });
-
-        Button button3 = new Button(this);
-        button3.setText("zamknieta seria 3");
-        button3.setBackground(getDrawable(R.drawable.gradient));
-        button3.setTextSize(20);
-        button3.setTextColor(getColor(R.color.colorText));
-        button3.setHeight(70);
-        button3.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
-        button3.setPadding(40, 20, 20, 20);
-        scrollLayout.addView(button3);
-
-        button3.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                if (newSV.getVisibility() == View.GONE)
-                {
-                    newSV.setVisibility(View.VISIBLE);
-                    button2.setText("pojawiam sie");
-                } else
-                {
-                    newSV.setVisibility(View.GONE);
-                    button2.setText("i znikam ");
+                    newLL.setPadding(0, 0, 0, 20);
                 }
             }
-        });
+            else
+            {
+                TextView emptyFolder = new TextView(comic.getContext());
+                emptyFolder.setText(R.string.resource_error);
+                emptyFolder.setTextSize(16);
+                emptyFolder.setTextColor(getColor(R.color.colorError));
+                emptyFolder.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                emptyFolder.setPadding(0, 20, 0, 20);
+                newLL.addView(emptyFolder);
+            }
+
+            scrollLayout.addView(newSV, scrollLayout.indexOfChild(comic) + 1); //fix this adding a new divider!!
+            comic.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    if (newSV.getVisibility() == View.GONE)
+                    {
+                        newSV.setVisibility(View.VISIBLE);
+                    } else
+                    {
+                        newSV.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+    }
+
+    public void refreshApp(View view)
+    {
+        LinearLayout scrollLayout = findViewById(R.id.linearMain);
+        scrollLayout.removeAllViews();
+        comics.clear();
     }
 
     void firstTime()
@@ -186,8 +232,6 @@ public class MainActivity extends AppCompatActivity
             {
                 //the whole folder choosing magic happens here i guess
 
-                pickDirectory();
-
                 scrollLayout.removeAllViews();
 
                 firstTime = false;
@@ -198,56 +242,6 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    void loadContent()
-    {
-        Series temp;
-        List<String> temp2 = new ArrayList<String>();
-        comics.clear();
-        File directory = new File(Environment.getExternalStorageDirectory().toString() + comicDir);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-        {
-            try
-            {
-                File[] files = directory.listFiles();
-                File[] issues;
-
-                for (File file : files)
-                {
-                    temp = new Series();
-                    temp2.clear();
-                    issues = file.listFiles();
-                    temp.title = file.getName();
-                    for (File issue : issues)
-                    {
-                        temp2.add(issue.getName());
-                    }
-                    temp.issues = temp2;
-                    comics.add(temp);
-                }
-            } catch (Exception error)
-            {
-                Log.e("Error", error.getMessage());
-            }
-        }
-
-        Log.e("FOLDER CONT: ", String.valueOf(comics.size()));
-        for(Series ser : comics)
-        {
-            Log.e("INFO", ser.title + ", " + ser.issues.toString());
-        }
-    }
-
-    void pickDirectory()
-    {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED)
-        {
-
-//            ActivityCompat.requestPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE, ); figure this out
-        }
-
-    }
 
     void goToChapter(int number)
     {
@@ -262,7 +256,7 @@ public class MainActivity extends AppCompatActivity
 
         if (requestCode == 1)
         {
-            if (opened != null)
+            /*if (opened != null)
             {
                 if ((newLL.indexOfChild(opened) % 2 == 0))
                 {
@@ -272,7 +266,7 @@ public class MainActivity extends AppCompatActivity
                     opened.setBackgroundColor(getColor(R.color.colorAccent2));
                 }
                 opened = null;
-            }
+            }*/
         }
     }
 
--- iComicPRO/app/src/main/java/com/example/icomicpro/Series.java
+++ iComicPRO/app/src/main/java/com/example/icomicpro/Series.java
@@ -1,10 +1,15 @@
 package com.example.icomicpro;
 
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class Series
 {
     String title;
-    List<String> issues;
+    List<String> issues = new ArrayList<>();
 }
